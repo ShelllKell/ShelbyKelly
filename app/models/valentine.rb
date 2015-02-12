6e11ed735695d849b6742559c72576ef467c6efb
Shelby Kelly <shelby.kelly@colorado.edu>
2015-02-12 15:42:02 -0700

    fixed length

--- app/models/valentine.rb
+++ app/models/valentine.rb
@@ -1,5 +1,5 @@
 class Valentine < ActiveRecord::Base
 
-  validates :message, length: {:minimum => 4, :maximum => 500, too_short: "must be more than 4 characters."}
+  validates :message, length: {:minimum => 4, :maximum => 2000, too_short: "must be more than 4 characters."}
 
 end
