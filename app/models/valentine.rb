399e5802f4fb5803313a94577841284abe1b831f
Shelby Kelly <shelby.kelly@colorado.edu>
2015-02-12 16:06:19 -0700

    error message

--- app/models/valentine.rb
+++ app/models/valentine.rb
@@ -1,5 +1,5 @@
 class Valentine < ActiveRecord::Base
 
-  validates :message, length: {:minimum => 4, :maximum => 2000, too_short: "must be more than 4 characters."}
+  validates :message, length: {:maximum => 2000, too_long: "That's too long of a love letter! Make it less than 500 words :)"}
 
 end
